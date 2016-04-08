749e60de6e7d676c1c486c95b9e6af3635425f11
Fady Makram <fmakram1@gmail.com>
2016-04-08 12:18:08 -0400

    Refactored track.py using ctypes instead of external C program

--- track.py
+++ track.py
@@ -2,9 +2,11 @@ import time
 import subprocess
 from collections import Counter
 import json
+import ctypes
+
+user32 = ctypes.windll.user32
 
 RESOLUTION = 3
-GET_WINDOW_EXEC = 'get_window.exe'
 WRITE_INTERVAL = 10 # write to file every WRITE_INTERVAL * RESOLUTION seconds
 STATS_FILE = 'stats.json'
 
@@ -13,11 +15,11 @@ def main ():
 	iteration = 0
 	
 	while True:
-		output = subprocess.check_output(GET_WINDOW_EXEC).decode("utf-8")
-		if output in windows:
-			windows[output] = windows[output] + RESOLUTION
+		window_title = get_window_title()
+		if window_title in windows:
+			windows[window_title] = windows[window_title] + RESOLUTION
 		else:
-			windows[output] = RESOLUTION
+			windows[window_title] = RESOLUTION
 				
 		if iteration == WRITE_INTERVAL:
 			with open(STATS_FILE, 'r') as f:
@@ -34,6 +36,14 @@ def main ():
 			time.sleep(RESOLUTION)
 		
 		iteration = iteration + 1
+
+def get_window_title():
+    hwnd = user32.GetForegroundWindow()
+    title_length = user32.GetWindowTextLengthW(hwnd)
+    title = ctypes.create_unicode_buffer(title_length + 1)
+    user32.GetWindowTextW(hwnd, title, title_length + 1)
+
+    return title.value
 
 if __name__ == '__main__':
 	main()
